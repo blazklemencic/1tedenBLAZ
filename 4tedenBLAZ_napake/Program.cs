eaa611da64371f014a93919b568c7b530217203e
VSŠ <VS@DESKTOP-2JC9998>
2025-10-27 17:29:08 +0100

    vrenebsjka evidenca + napake

---
+++ 4tedenBLAZ_napake/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace _4tedenBLAZ_napake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            napaka_1();
+            napaka_2();
+            napaka_3();
+            napaka_4();
+            napaka_5();
+            napaka_6();
+            napaka_7();
+            napaka_8();
+            napaka_9();
+        }
+
+        static void napaka_1()
+        {
+            // WriteLine, ime
+            string ime = "Miroslav";
+            Console.WriteLine("Pozdravljen, " + ime + "!");
+        }
+
+        static void napaka_2()
+        {
+            // ne mores deliti z 0
+            int a = 10;
+            int b = 0;
+            if (b == 0)
+            {
+                Console.WriteLine("dont divide by 0");
+                return;
+            }
+            Console.WriteLine("Rezultat je: " + (a / b));
+        }
+
+        static void napaka_3()
+        {
+            // Naloga: Preveri, ali je številka soda ali liha
+            // sintaksa + obrnjena logika
+            int stevilo = 5;
+            if (stevilo % 2 == 0)
+            {
+                Console.WriteLine("Številka je soda.");
+            }
+            else
+            {
+                Console.WriteLine("Številka je liha.");
+            }
+        }
+
+        static void napaka_4()
+        {
+            // Naloga: Prikaz temperature glede na vhodni podatek
+            // sintaksa
+            int temperatura = 20;
+            if (temperatura < 15)
+            {
+                Console.WriteLine("Mrzlo je.");
+            }
+            else
+            {
+                Console.WriteLine("Toplo je.");
+            }
+        }
+
+        static void napaka_5()
+        {
+            // Naloga: Izpiši številke od 1 do 5 vključno z 1 in 5
+            // manjka }
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Številka: " + i);
+            }
+        }
+
+        static void napaka_6()
+        {
+            // Naloga: Pretvori decimalno število v celo število
+            // (int) in wrong case
+            double stevilo = 12.5;
+            int celoStevilo = (int)stevilo;
+            Console.WriteLine("Celo število: " + celoStevilo);
+        }
+
+        // Naloga: Kreiraj metodo za seštevanje dveh števil
+        // en parameter premalo
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void napaka_7()
+        {
+            Console.WriteLine("Rezultat je: " + Add(5, 7));
+        }
+        
+        static void napaka_8()
+        {
+            // Naloga: Poišči določeno vrednost v tabeli
+            // manjka en =
+            int[] tabela = { 1, 2, 3, 4, 5 };
+            int iskanaVrednost = 3;
+            for (int i = 0; i < tabela.Length; i++)
+            {
+                if (tabela[i] == iskanaVrednost)
+                {
+                    Console.WriteLine("Vrednost najdena!");
+                }
+            }
+        }
+
+        static void napaka_9()
+        {
+            // Naloga: Preveri, ali je vnos uporabnika številka
+            // Console.ReadLine(), TryParse, obrnjena logika
+            Console.WriteLine("Vnesite število:");
+            string? vnos = Console.ReadLine();
+            bool jeStevilo = int.TryParse(vnos, out _);
+            if (jeStevilo == true)
+            {
+                Console.WriteLine("Vnesena vrednost je številka.");
+            }
+            else
+            {
+                Console.WriteLine("Napaka: Vnesena vrednost ni številka.");
+            }
+        }
+    }
+}
---
+++ 4tedenBLAZ_vremenskaevidenca/Program.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+
+namespace _4tedenBLAZ_vremenskaevidenca
+{
+    enum OpisVremena
+    {
+        soncen,
+        zasnezen,
+        dezeven,
+        vetroven,
+        megleno,
+        toplo
+    }
+    internal class VremenskaEvidenca
+    {
+
+
+        private readonly static List<Vreme> vremes = new List<Vreme>();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Za koliko dni zelite vnesti vreme? ");
+            string? steviloDniString = Console.ReadLine();
+            bool jeStevilo = int.TryParse(steviloDniString, out int stDni);
+            if (!jeStevilo)
+            {
+                Console.WriteLine("Ni stevilo!");
+                return;
+            }
+
+            Array opisi = Enum.GetValues(typeof(OpisVremena));
+            string opisiVremena = "";
+            for (int i = 0; i < opisi.Length; i++)
+            {
+                opisiVremena += opisi.GetValue(i);
+
+                if (i < opisi.Length - 1)
+                    opisiVremena += ", ";
+            }
+            Console.WriteLine($"Za vsak dan vnesi vreme v formatu: \"[temperatura] [opis (moznosti so: {opisiVremena})]\"");
+            for (int i = 1; i <= stDni; i++)
+            {
+                Console.Write($"{i}. Dan: ");
+                string? vneseniString = Console.ReadLine();
+                if (vneseniString == null)
+                {
+                    Console.WriteLine("string je null??");
+                    i--;
+                    continue;
+                }
+                string[] splitString = vneseniString.Split(" ");
+                if (splitString.Length != 2)
+                {
+                    Console.WriteLine("Format vremena za dan ni pravilen. Znova si poglejte navodila formata v zgornjih navodilih.");
+                    i--;
+                    continue;
+                }
+                bool temperaturaJeStevilo = float.TryParse(splitString[0], out float temperatura);
+                if (!temperaturaJeStevilo)
+                {
+                    Console.WriteLine("Temperatura ni stevilo! poskusi znova");
+                    i--;
+                    continue;
+                }
+
+                bool niPravilenOpis = false;
+                foreach (OpisVremena o in opisi)
+                {
+                    if (splitString[1].Contains(o.ToString()))
+                    {
+                        niPravilenOpis = false;
+                        break;
+                    }
+                    else
+                    {
+                        niPravilenOpis = true;
+                    }
+                }
+                bool jeOpis = OpisVremena.TryParse(splitString[1], out OpisVremena opis);
+                if (!jeOpis || niPravilenOpis == true)
+                {
+                    Console.WriteLine("opis ni v pravem formatu! poskusi znova");
+                    i--;
+                    continue;
+                }
+
+                Vreme vreme = new Vreme(i, temperatura, opis);
+
+                vremes.Add(vreme);
+            }
+
+            double averageTemp = averageTemperature(vremes);
+            averageTemp = Math.Round(averageTemp, 2);
+            Vreme biggestTemp = najvisjaTemperatura(vremes);
+            Vreme lowestTemp = najnizjaTemperatura(vremes);
+            Console.Clear();
+            Console.WriteLine("============== REZULTATI ===============");
+            foreach (Vreme v in vremes)
+            {
+                double temperatura = Math.Round(v.getTemperatura(), 2);
+                int dan = v.getDan();
+                OpisVremena opis = v.getOpisVremena();
+                Console.WriteLine($"{dan}. dan: {temperatura}°C, {opis}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Povprecna temperatura za vse dni: {averageTemp}°C");
+            Console.WriteLine($"Najvisja temperatura: {biggestTemp.getDan()}. dan: {biggestTemp.getTemperatura()}°C");
+            Console.WriteLine($"Najnizja temperatura: {lowestTemp.getDan()}. dan: {lowestTemp.getTemperatura()}°C");
+            Console.WriteLine("========================================");
+
+            Console.ReadLine();
+        }
+
+        private static float averageTemperature(List<Vreme> v)
+        {
+            if(v == null)
+            {
+                Console.WriteLine("List of vremes is null");
+                return 0f;
+            }
+
+            float sum = 0f;
+            foreach (Vreme vreme in v)
+            {
+                sum += vreme.getTemperatura();
+            }
+            return sum / v.Count;
+        }
+
+        private static Vreme najvisjaTemperatura(List<Vreme> v)
+        {
+            Vreme vreme = v.Aggregate((i1, i2) => i1.getTemperatura() > i2.getTemperatura() ? i1 : i2);
+            return vreme;
+        }
+
+        private static Vreme najnizjaTemperatura(List<Vreme> v)
+        {
+            Vreme vreme = v.Aggregate((i1, i2) => i1.getTemperatura() < i2.getTemperatura() ? i1 : i2);
+            return vreme;
+        }
+    }
+
+    internal class Vreme
+    {
+        readonly OpisVremena opis;
+        readonly float temperatura;
+        readonly int dan;
+
+        public Vreme(int dan, float temperatura, OpisVremena opis)
+        {
+            this.opis = opis;
+            this.temperatura = temperatura;
+            this.dan = dan;
+        }
+
+        public OpisVremena getOpisVremena()
+        {
+            return this.opis;
+        }
+
+        public float getTemperatura()
+        {
+            return this.temperatura;
+        }
+
+        public int getDan()
+        {
+            return this.dan;
+        }
+    }
+}
